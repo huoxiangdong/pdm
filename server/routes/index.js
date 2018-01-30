e71d9b93a9739eae2b2b3f280988bd048a267656
huoxiangdong <huoxiangdong@aliyun.com>
2018-01-30 09:29:06 +0800

    添加登录、注册

--- server/routes/index.js
+++ server/routes/index.js
@@ -15,7 +15,29 @@ let createToken = name => {
         expiresIn: '24h'
     })
 }
-
+// 检查token
+let checkToken = (ctx,next) => {
+    if (ctx,request.headers['authorization']) {
+        let token = ctx.request.headers['authorization'].split(' ')
+        // 结构token， 生成一个对象 { name: xx, iat: xx, exp: xx}
+        let decoded = jwt.decode(token)
+        // 检查 token 是否过期，decoded.exp为创建时间，增加设置过期时间
+        if (token && decoded.exp <= Data.now() / 1000 ) {
+            ctx.response.status = 401
+            ctx.token = {
+                success: false,
+                token: false,
+                message: '登录过期，请重新登录'
+            }   
+        }else {
+            ctx.token = {
+                success: true,
+                token: true
+            }
+        }
+    }
+    next()
+}
 // 返回结果初始
 let result = {
     success: false,
