288ceb79b11ca95de2db2aaf975d6f4364f66b2d
huoxiangdong <huoxiangdong@aliyun.com>
2018-01-31 13:42:09 +0800

    登录完成

--- server/routes/index.js
+++ server/routes/index.js
@@ -16,7 +16,7 @@ let createToken = name => {
     })
 }
 // 检查token
-let checkToken = (ctx,next) => {
+/* let checkToken = (ctx,next) => {
     if (ctx,request.headers['authorization']) {
         let token = ctx.request.headers['authorization'].split(' ')
         // 结构token， 生成一个对象 { name: xx, iat: xx, exp: xx}
@@ -37,7 +37,7 @@ let checkToken = (ctx,next) => {
         }
     }
     next()
-}
+} */
 // 返回结果初始
 let result = {
     success: false,
@@ -53,7 +53,64 @@ router.get('/', (ctx, next) => {
 router.get('/api', (ctx) => {
     ctx.body = "FUCK!!!!!"
 })
-// 注册
+// 检查token  ajax --> 拦截请求，为header添加Authorization
+/* router.get('/api/token',async (ctx, next) => {
+    console.log('到这里了')
+    await if (ctx.request.headers['Authorization']) {
+        
+      let token = ctx.request.headers['Authorization'].split(' ')[1]
+      // 解构 token，生成一个对象 { name: xx, iat: xx, exp: xx }
+      let decoded = jwt.decode(token)
+      // console.log(decoded)
+      // 监测 token 是否过期，decoded.exp为创建时时间 ➕ 增加设置的过期时间
+      if (token && decoded.exp <= Date.now() / 1000) {
+        ctx.response.status = 401
+        ctx.token = {
+          success: false,
+          token: false,
+          message: 'token过期，请重新登录'
+        }
+      } else {
+        console.log(ctx.request.headers)
+        ctx.token = {
+          success: true,
+          token: true
+        }
+      }
+    }
+    ctx.body = ctx.token
+  }) */
+
+  
+  const checkToken = (ctx, next) => {
+    if (ctx.request.headers['authorization']) {
+      let token = ctx.request.headers['authorization'].split(' ')[1]
+      // 解构 token，生成一个对象 { name: xx, iat: xx, exp: xx }
+      let decoded = jwt.decode(token)
+      // console.log(decoded)
+      // 监测 token 是否过期，decoded.exp为创建时时间 ➕ 增加设置的过期时间
+      if (token && decoded.exp <= Date.now() / 1000) {
+        ctx.response.status = 401
+        ctx.token = {
+          success: false,
+          token: false,
+          message: 'token过期，请重新登录'
+        }
+      } else {
+        ctx.token = {
+          success: true,
+          token: true
+        }
+      }
+    }
+    next()
+  }
+  const getToken = async ctx => {
+    ctx.body = ctx.token
+  }
+
+  router.get('/api/token', checkToken, getToken)
+  // 注册
 router.post('/api/register', async (ctx) => {
     
     let req = ctx.request.body
@@ -88,17 +145,17 @@ router.post('/api/register', async (ctx) => {
 }) // 注册
 
 // 登录
-router.post('/api/login',async (ctx) => {
-   let req = ctx.request.body 
-   await user.findOne({ name: req.name})
-             .then(data => {
-                 if (!data){
-                     result.message = '账号不存在'
+router.post('/api/login', async (ctx) => {
+   let requestBody = ctx.request.body 
+   await user.findOne({ name: requestBody.name})
+             .then(user => {
+                 if (!user){
+                    result.message = '账号不存在'
                      ctx.body = result 
-                 }else if (req.password === data.password) {
+                 }else if (requestBody.password === user.password) {
                      result.success = true
                      result.message = '登录成功'
-                     result.data = { name: data.name,token: createToken(data.name)}
+                     result.data = { name: user.name,token: createToken(user.name)}
                  }else { result.message = '密码错误'}
              }).catch(err => { ctx.body = err})
              ctx.body = result
